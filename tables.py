7f063369bfb095944ef340c8366b0a7d9fae3791
Vladimir Panferov <v3nseagal@gmail.com>
2022-01-22 02:15:16 +0700

    Creating DB tables

---
+++ tables.py
@@ -0,0 +1,46 @@
+from sqlalchemy import (
+    Column,
+    Date,
+    ForeignKey,
+    Integer,
+    String
+)
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    FIO = Column(String, unique=True)
+    avatar = Column(String, unique=True, nullable=True)
+    sex = Column(String)
+    birthdate = Column(Date)
+    address = Column(String, nullable=True)
+
+
+class Phone(Base):
+    __tablename__ = "phones"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"), index=True)
+    kind = Column(String)
+    phone_number = Column(String)
+
+    user = relationship("User", backref="phones")
+
+
+class Email(Base):
+    __tablename__ = "emails"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"), index=True)
+    kind = Column(String)
+    email = Column(String)
+
+    user = relationship("User", backref="emails")
